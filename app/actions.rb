1fb7b10dbe2ae0db6d4397e0d503ac72b761705c
Sarah Clinton-Baker <sarahclintonbaker@gmail.com>
2015-04-24 04:05:53 +0000

    updated email
    actions updated

--- app/actions.rb
+++ app/actions.rb
@@ -1,12 +1,15 @@
 # Homepage (Root path)
 get '/' do
+  erb :index
+end
+
+get '/memory' do
   erb :memory
 end
 
 post '/selection' do
   @client = GooglePlaces::Client.new("AIzaSyCoKTsUyTkHVTRQIcDNxZkPrwL-aunhQL4")
 
-  @location = Geocoder.search("70.36.63.26")
   latitude = @location[0].data["latitude"]
   longitude = @location[0].data["longitude"]
 
