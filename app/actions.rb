7f8531e4f3231fa0a60c6021144d2ba373be6169
vagrant <vagrant@lighthouse-labs>
2015-04-24 02:07:11 +0000

    javascript gps added

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,14 @@
 # Homepage (Root path)
 get '/' do
-  erb :index
+  erb :memory
+end
+
+post '/selection' do
+  @client = GooglePlaces::Client.new("AIzaSyCoKTsUyTkHVTRQIcDNxZkPrwL-aunhQL4")
+
+  @location = Geocoder.search("70.36.63.26")
+  latitude = @location[0].data["latitude"]
+  longitude = @location[0].data["longitude"]
+
+  @spots = @client.spots(latitude, longitude, :name => @meal, :radius => @radius)
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
 
+  if (navigator.geolocation) {
+    console.log('Geolocation is supported!');
+  }
+  else {
+    console.log('Geolocation is not supported for this Browser/OS version yet.');
+  }
+});
+
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
