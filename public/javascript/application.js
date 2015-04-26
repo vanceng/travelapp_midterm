599bedbdf3ec9292bfe76259fe9381c5906dd1cf
Sakthi Pravag <sakthipravag@gmail.com>
2015-04-26 02:03:57 +0000

    Updated maps

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -8,15 +8,33 @@ $(document).ready(function() {
 
         // Create the coordinate of the memory
         var coords = new google.maps.LatLng(response.latitude, response.longitude);
-       
+        var time = response.created_at
         // Create and display the map
+        var contentString = '<div id="content">'+
+          '<div id="siteNotice">'+
+          '</div>'+
+          //'<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+          '<div id="bodyContent">'+
+          '<p><b>Hello</b></p>'+
+          '</div>'+
+          '</div>';
 
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+          });
+
+    
         // Display the markers on the map
         var marker = new google.maps.Marker({
           position: coords,
-          animation:google.maps.Animation.BOUNCE
+          //animation:google.maps.Animation.BOUNCE
         });
         marker.setMap(map);
+
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.open(map,marker);
+        });
 
       }
     });
@@ -24,11 +42,131 @@ $(document).ready(function() {
 
   var memoryIds = $(".memories").data("memory-ids");
   // create the map
+  var styles = [
+     {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 65
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 51
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 30
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 40
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.province",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": -25
+            },
+            {
+                "saturation": -100
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#ffff00"
+            },
+            {
+                "lightness": -25
+            },
+            {
+                "saturation": -97
+            }
+        ]
+    }
+  ];
+
 
   var mapProp = {
     center: new google.maps.LatLng(49.282022399999995, -123.108199),
-    zoom: 10,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
+    zoom: 15,
+    mapTypeId:google.maps.MapTypeId.ROADMAP,
+    styles: styles
   };
   var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
